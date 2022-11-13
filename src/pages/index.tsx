ab4556668852681d079320abf1b9b14f09638534
Bruno Martins <brunomart97@hotmail.com>
2022-11-13 17:03:31 -0300

    Style setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,12 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
-  return <h1>Contagem: {props.count}</h1>
+  return <h1 className="text-violet-500">Contagem: {props.count}</h1>
 }
 
 export const getServerSideProps = async () => {
   const response = await fetch('http://localhost:3333/pools/count')
   const data = await response.json()
-
-  console.log(data)
 
   return {
     props: {
