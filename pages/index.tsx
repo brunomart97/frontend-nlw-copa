75e2b4fbcecb9ff63d98cb063013894d28eb5658
Bruno Martins <brunomart97@hotmail.com>
2022-11-13 09:19:28 -0300

    API access

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
-  return (
-    <h1>Hello World</h1>
-  )
+interface HomeProps {
+  count: number
+}
+
+export default function Home(props: HomeProps) {
+  return <h1>Contagem: {props.count}</h1>
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  console.log(data)
+
+  return {
+    props: {
+      count: data.count
+    }
+  }
 }
